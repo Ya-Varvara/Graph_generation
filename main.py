91b0f075436bb3aa49ecc4cdb73f14783071a99a
Yakovleva Varvara <yakovleva.varvaraa@gmail.com>
2022-11-04 11:26:10 +0300

    *

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-from generation.net_generation import generate
+# from generation.net_generation import generate, check_graph_flow, generate_graph_base, make_strongly_connected, make_flow, make_cut, make_cut_2
+# from db.db_handler import add_graph
+# from generation.algorithms import FordFulkerson
+# from generation.graph_draw import draw_graph
+import sqlite3
 
+db_file_name = 'graph.db'
+db = sqlite3.connect(db_file_name)
+c = db.cursor()
+c.execute("""DROP TABLE graphs""")
+c.execute("""DROP TABLE folders""")
+db.commit()
+c.close()
 
-generate(25)
+# n = 12
+#
+# graph = generate_graph_base(n)
+# draw_graph(graph)
+# make_strongly_connected(graph)
+# draw_graph(graph)
